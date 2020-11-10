73a814d624c463d6b5283c9f0b0a7ed8cc72a7f0
Martinique <me@martidolce.com>
2020-11-10 12:51:19 -0500

    Made some significant changes to this directory structure.

---
+++ lib/introduction.rb
@@ -0,0 +1,20 @@
+=begin
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+=end
+
+# lib/introduction.rb
+
+#introduction
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+#introduction("Marti")
+
+#introduction_with_language
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
